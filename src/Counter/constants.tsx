fec3ce7824db565bc581748ff5fd311a564f3fc1
Anton Kolesnik <kolesnikanton@icloud.com>
2023-04-13 09:53:16 +0200

    Update packages. Error component

---
+++ src/Counter/constants.tsx
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGES = {
+  smallDurationValue: 'TOO SMALL DURATION VALUE',
+  emptyStartEndValue: 'START & END VALUES ARE EMPTY',
+};
+
+export const SMALL_DURATION_VALUE = 800;
--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -7,11 +7,12 @@ import './index.css';
 
 const Demo = () => (
   <div className="demo">
-    <Counter start={100} duration={10000} />
-    <Counter end={500} duration={10000} />
-    <Counter duration={10000} />
-    <Counter start={100} end={200.5} duration={30000} />
-    <Counter start={1.2} end={10.5} duration={10000} />
+    <Counter fontSize={50} withAnimation start={200} end={100} duration={100000} />
+    <Counter fontSize={50} end={500} duration={10000} />
+    <Counter fontSize={50} withAnimation start={100} end={200} duration={100000} />
+    <Counter fontSize={50} start={100.2} end={50.5} duration={500000} />
+    <Counter fontSize={50} start={1.2} end={10.5} duration={10000} />
+    {/* <Counter fontSize={50} duration={10000} /> */}
   </div>
 );
 
