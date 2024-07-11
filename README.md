1e2a8dd043117570921ccfbb88b1424d14cd32a8
Anton Kolesnik <kolesnikanton@icloud.com>
2024-07-11 09:35:38 +0200

    coverage. readme

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ React number increase/decrease animation
 * Without repeat
 
 ## Demo
-See demo [HERE](https://antonkolesnik.com/counter/)
+See demo [HERE](https://antonkolesnik.com/counter)
 
-## Props:
+## Props
 * `start (0 by default)`: A number to start the counter at
 * `end (0 by default)`: A number to stop the counter at
 * `duration (required)`: Time in ms to stop the counter
@@ -36,19 +36,19 @@ yarn add @antonkolesnik/counter
 import '@antonkolesnik/counter/lib/styles.css';
 ```
 
-## Example:
+## Usage
 ```js
 import Counter from '@antonkolesnik/counter';
 import '@antonkolesnik/counter/lib/styles.css';
 
 const Demo = () => (
   <div>
-    <Counter fontSize={50} start={100} duration={10000} />
-    <Counter end={500} duration={10000} />
-    <Counter start={200} end={50} duration={10000} />
-    <Counter start={100} end={200} duration={10000} />
-    <Counter start={1.2} end={10.5} duration={10000} />
-    <Counter withAnimation fontSize={80} start={200} end={100} duration={100000} />
+    <Counter start="100" duration={10000} fontSize={50} />
+    <Counter end="500" duration={10000} />
+    <Counter start="200" end="50" duration={10000} />
+    <Counter start="100" end="200" duration={10000} />
+    <Counter start="1.20" end="10.5" duration={10000} decimals={1} />
+    <Counter start="200" end="100" duration={100000} fontSize={80} withAnimation />
   </div>
 );
 ```
--- src/Counter/__tests__/index.test.tsx
+++ src/Counter/__tests__/index.test.tsx
@@ -17,14 +17,17 @@ test('render', async () => {
   const input2 = container.getElementsByClassName('current')[0] as HTMLDivElement;
   expect(input2.innerText).toBe('23.5');
 
+  const nextEl = container.getElementsByClassName('next')[0] as HTMLDivElement;
+  expect(nextEl.innerText).toBe(undefined);
+
   expect(container).toMatchSnapshot();
 });
 
-test('render - with animation', async () => {
+test('render - withAnimation', async () => {
   jest.useFakeTimers();
 
   const { container } = render(
-    <Counter fontSize={80} start="200" end="100" duration={100000} />,
+    <Counter withAnimation fontSize={80} start="200" end="100" duration={100000} />,
   );
 
   const input1 = container.getElementsByClassName('current')[0] as HTMLDivElement;
@@ -32,8 +35,74 @@ test('render - with animation', async () => {
 
   jest.runAllTimers();
 
+  const nextEl = container.getElementsByClassName('next')[0] as HTMLDivElement;
+  expect(nextEl.innerText).toBe('100');
+
+  const currentEl = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(currentEl.innerText).toBe('101');
+
+  expect(container).toMatchSnapshot();
+});
+
+test('render with floats', async () => {
+  jest.useFakeTimers();
+
+  const { container } = render(
+    <Counter fontSize={80} start="10.1" end="100.10" duration={100000} />,
+  );
+
+  const input1 = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(input1.innerText).toBe('10.1');
+
+  jest.runAllTimers();
+
+  const input2 = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(input2.innerText).toBe('100.10');
+
+  const nextEl = container.getElementsByClassName('next')[0] as HTMLDivElement;
+  expect(nextEl.innerText).toBe(undefined);
+
+  expect(container).toMatchSnapshot();
+});
+
+test('render with floats with decimals', async () => {
+  jest.useFakeTimers();
+
+  const { container } = render(
+    <Counter decimals={2} fontSize={80} start="10.10000" end="100.10000000" duration={100000} />,
+  );
+
+  const input1 = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(input1.innerText).toBe('10.10000');
+
+  jest.runAllTimers();
+
+  const input2 = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(input2.innerText).toBe('100.10');
+
+  const nextEl = container.getElementsByClassName('next')[0] as HTMLDivElement;
+  expect(nextEl.innerText).toBe(undefined);
+
+  expect(container).toMatchSnapshot();
+});
+
+test('render without start', async () => {
+  jest.useFakeTimers();
+
+  const { container } = render(
+    <Counter fontSize={80} end="100" duration={100000} />,
+  );
+
+  const input1 = container.getElementsByClassName('current')[0] as HTMLDivElement;
+  expect(input1.innerText).toBe('0');
+
+  jest.runAllTimers();
+
   const input2 = container.getElementsByClassName('current')[0] as HTMLDivElement;
   expect(input2.innerText).toBe('100');
+
+  const nextEl = container.getElementsByClassName('next')[0] as HTMLDivElement;
+  expect(nextEl.innerText).toBe(undefined);
 
   expect(container).toMatchSnapshot();
 });
